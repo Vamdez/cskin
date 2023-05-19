9365d35e20a870eeb7b686f8a0f175fa42b6f63d
Victor Almeida Morais <vamdez123@gmail.com>
2023-05-19 17:58:06 -0300

    firs commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+
+
+app.use('/', express.static(__dirname + '/src'));
+
+app.listen(5000, ()=>{
+    console.log("Server Runnig")
+});
