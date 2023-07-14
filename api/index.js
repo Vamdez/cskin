b89add2de563edc4c2b39ab8514820e39fb8dfd1
Victor Almeida Morais <vamdez123@gmail.com>
2023-07-14 18:02:01 -0300

    change

--- api/index.js
+++ api/index.js
@@ -1,16 +1,5 @@
-async()=>{
-const db = require('./models/db');
-const Pattern_Table = require("./models/patterns");
-const Skin_Table = require("./models/skins");
-try{
-    const resultado = await db.sync();
-    console.log(resultado);
-}
-catch(error){
-    console.log(error);
-}
-};
 
+const db = require('./models/db');
 const express = require("express");
 const app = express();
 
--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
 
+app.set('view engine', 'ejs');
 
+app.use('/public', express.static('public', {
+    extensions: ['html','css','js']
+}));
 
-app.use('/', express.static(__dirname + '/src'));
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
-
-app.listen(5000, ()=>{
-    console.log("Server Runnig")
-    
+app.listen(5000, () => {
+  console.log("Server Running");
 });
