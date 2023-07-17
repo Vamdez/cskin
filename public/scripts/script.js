4cd486a24c5f94758acd607d16a4cdcaf76357f7
Victor Almeida Morais <vamdez123@gmail.com>
2023-07-17 18:41:45 -0300

    login-p1

---
+++ public/scripts/script.js
@@ -0,0 +1,9 @@
+const button_login = document.getElementById("login-button");
+const menu_login = document.getElementById("menu-login");
+
+button_login.addEventListener('click', function(){
+    menu_login.style.display = "block";
+});
+
+const form_login = document.getElementById("form-login");
+const submit_login = document.getElementById("submit");
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+
 app.set('view engine', 'ejs');
 
 app.use('/public', express.static('public', {
@@ -10,6 +12,13 @@ app.use('/public', express.static('public', {
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.post('/dados', (req, res) => {
+  const email = req.body.email;
+  const senha = req.body.password;
+  res.send("Formulário enviado com sucesso. Email: " + email);
+});
+
 
 app.listen(5000, () => {
   console.log("Server Running");
